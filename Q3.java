b9ae8983fb1ab742e970a1c1518c0dfe7c583382
yngridandrade26 <leidianacav@gmail.com>
2025-10-24 20:21:21 -0300

    Update Q3.java

---
+++ Q3.java
@@ -0,0 +1,13 @@
+public class Operaçoes{
+    public static void main (string [] args){
+        public static void main (string [] args)
+        int n1 = 7;
+        int n2 = 3;
+        int soma = (n1+n2);
+        int subtração = (n1+n2);
+        system.out.println("Os números são: " + "n1" + "n2");
+        system.out.println("A soma é: " + "soma");
+        system.out.println("A subtração é: " + "subtração");
+    }
+}
+    
