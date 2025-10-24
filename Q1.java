560ef7c2ec0cece6144bf97beb620c2d43caea55
yngridandrade26 <leidianacav@gmail.com>
2025-10-24 20:20:35 -0300

    Update Q1.java

---
+++ Q1.java
@@ -0,0 +1,8 @@
+public class frasidade{
+    public static void main (string [] args){
+    string nome = ("Yngrid Maria");
+    string frase = ("Nunca desista!");
+    int idade = 15;
+    system.out.println("A frase é:"  "frase" + "idade" + "nome");
+    }
+}
